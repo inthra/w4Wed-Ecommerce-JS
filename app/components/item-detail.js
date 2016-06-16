ac190d517fc329d43f636a018480e4f118201a4e
In Mahaphol and Nadiya Yeroshkina <nadiya@>
2016-06-16 11:01:44 -0700

    add shopping cart, rout to it,can display added items.

--- app/components/item-detail.js
+++ app/components/item-detail.js
@@ -5,6 +5,9 @@ export default Ember.Component.extend({
   actions: {
     update(item, params) {
       this.sendAction('update', item, params);
+    },
+    destroyItem(item) {
+      this.sendAction('destroyItem', item);
     }
   }
 });
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('admin');
+  this.route('cart');
 });
 
 export default Router;
